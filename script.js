221ebfceee03d2006dc9420c683fb1674ca74d3c
nandhakumar.n <srinanjundesvarar888@gmail.com>
2023-06-14 12:45:27 +0530

    update all

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle Navbar on Click
+document.querySelector(".navbar-toggler").addEventListener("click", function() {
+    document.querySelector(".navbar").classList.toggle("active");
+  });
+  
